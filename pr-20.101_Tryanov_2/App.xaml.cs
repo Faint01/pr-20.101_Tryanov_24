0cde7852f1b3f636280e300f01dec66f16dc8591
kteys <kteys@KIRILL>
2023-03-31 11:27:03 +0300

    Добавьте файлы проекта.

---
+++ pr-20.101_Tryanov_2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace pr_20._101_Tryanov_2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ pr-20.101_Tryanov_2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using pr_20._101_Tryanov_2.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr_20._101_Tryanov_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Entities db = new Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bd_Teac_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+
+        }
+
+        private void Windows_Loaded(object sender, RoutedEventArgs e)// вывод данных на экран
+        {
+            var query =
+                from Teachers in db.Teachers
+                where Teachers.IdSpeciality == 1
+                orderby Teachers.FirstName 
+                select new { Teachers.IdTeachers, Teachers.LastName, Teachers.FirstName, Teachers.Patronymic, Teachers.Email, Teachers.IdStatusTeachers, Teachers.IdRole, Teachers.IdSpeciality };
+
+            bd_Teac.ItemsSource = query.ToList();
+        }
+    }
+}
